74f37dba8c6b8093bd4f7979ef99c7bdcc736ea6
wsepesi <sepesi.w@wustl.edu>
2023-12-26 21:11:11 -0600

    shadcn build error fix

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,5 +1,5 @@
-import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react"
 
 import { cn } from "lib/utils"
 
@@ -17,7 +17,7 @@ const Progress = React.forwardRef<
   >
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
